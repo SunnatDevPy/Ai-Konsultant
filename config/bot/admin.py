6c90ce74ccaeea5b00d799311acd9467e564958c
Sardorbek <nsardorbek776@gmail.com>
2025-03-03 16:12:41 +0500

    Project created

---
+++ config/bot/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from bot.models import User, Certification, Kpi, File, Bonus
+
---
+++ config/bot/models.py
@@ -0,0 +1,5 @@
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from decimal import Decimal
+from django.db import models
+
