f3ce314946af2b7ef9464d63744e4989027f8338
Sardorbek Norqulov <152975289+Norqulov-Sardorbek@users.noreply.github.com>
2025-03-04 21:34:29 +0500

    Delete config directory

--- config/bot/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from bot.models import UniversityApplication,TemporaryUser,StudyDirections
-
-
-admin.site.register(UniversityApplication)
-admin.site.register(TemporaryUser)
-admin.site.register(StudyDirections)
--- config/start-celery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Start Celery worker in the background
-celery celery --app=config worker --loglevel=info   &
-
-# Start Celery beat
-celery -A worker beat --loglevel=info
